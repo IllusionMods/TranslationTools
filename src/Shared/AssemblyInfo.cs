999b47cfcc0d6190900f082d717e330e405c5810
DeathWeasel1337 <deathweasel1337@gmail.com>
2020-03-05 18:45:38 -0600

    Update AssemblyInfo

--- src/Shared/AssemblyInfo.cs
+++ src/Shared/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/DeathWeasel1337/KK_Plugins")]
-[assembly: AssemblyCopyright("Copyright ©DeathWeasel1337 2019")]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/TranslationTools")]
+[assembly: AssemblyCopyright("Copyright ©IllusionMods 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
